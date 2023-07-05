d1645e17b19fb1a19c1a48e942308cfbcf880fcd
satinder <satindersingh472@gmail.com>
2023-07-05 14:13:17 -0600

    learned_plot_mean_mode_etc

--- matplotlib_practice.py
+++ matplotlib_practice.py
@@ -17,7 +17,7 @@ plt.title("Student Grades")
 plt.xlabel('Student')
 plt.ylabel('Grade')
 plt.grid(color="#95a5a6",linestyle='--', linewidth=2,axis='y',alpha=0.7)
-plt.show()
+# plt.show()
 
 fig,ax = plt.subplots(1,2,figsize=(10,4))
 # create a bar plot of name vs grade on the first axis
@@ -34,6 +34,60 @@ ax[1].legend(pass_counts.keys().tolist())
 fig.suptitle('Student Data')
 
 
-fig.show()
+# fig.show()
 
-# print(df_students)
+# get the variable to examine
+var_data = df_students['Grade']
+
+# create a figure
+fig = plt.figure(figsize=(10,4))
+
+#plot a histogram
+plt.hist(var_data)
+
+#add the titles and labels
+plt.title('Data Distribution')
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+
+# plt.show()
+# fig.show()
+
+# Measures of central tendency
+
+#save grades in a variable to calculate median , mode , mean
+var_grades = df_students['Grade']
+
+# get statitics
+min_val = var_grades.min()
+max_val = var_grades.max()
+mean_val = var_grades.mean()
+med_val = var_grades.median()
+mod_val = var_grades.mode()[0]
+
+#print out the statistics
+print('Minimum value: {:.2f}\nMaximum value: {:.2f}\nMean value: {:.2f}\nMedian value: {:.2f}\nMode value: {:.2f}\n'.format(min_val,max_val,mean_val,med_val,mod_val))
+
+# create a fig
+fig = plt.figure(figsize=(10,4))
+
+# plot a histogram
+
+plt.hist(var_grades)
+
+# add lines for the statistics
+
+plt.axvline(x=min_val, color = 'gray', linestyle='dashed', linewidth = 2)
+plt.axvline(x=mean_val, color = 'cyan', linestyle='dashed', linewidth = 2)
+plt.axvline(x=med_val, color = 'red', linestyle='dashed', linewidth = 2)
+plt.axvline(x=mod_val, color = 'yellow', linestyle='dashed', linewidth = 2)
+plt.axvline(x=max_val, color = 'gray', linestyle='dashed', linewidth = 2)
+
+# add title and labels
+
+plt.title("Data Distribution")
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+
+plt.show()
+fig.show()
