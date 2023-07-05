d2a9e5b61665f1eb5fc093edeb482b2459574974
satinder <satindersingh472@gmail.com>
2023-07-05 16:34:07 -0600

    worked_on_plot_density

---
+++ fuel_consumption.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('fuel_cons.csv',delimiter=',',header='infer')
+
--- matplotlib_practice.py
+++ matplotlib_practice.py
@@ -10,6 +10,72 @@ df_students = df_students.dropna(axis=0,how='any')
 passes = pd.Series(df_students['Grade'] >= 60)
 # concat the pass series at axis 1 
 df_students = pd.concat([df_students,passes.rename('Pass')],axis=1)
+
+def show_distribution(var_data):
+    min_val = var_data.min()
+    max_val = var_data.max()
+    mean_val = var_data.mean()
+    med_val = var_data.median()
+    mod_val = var_data.mode()[0]
+    print('Minimum value: {:.2f}\nMaximum value: {:.2f}\nMedian value: {:.2f}\nMean value: {:.2f}\nMode value: {:.2f}'.format(min_val,max_val,mean_val,med_val,mod_val))
+
+    # Create a figure for 2 subplots (2 rows, 1 column)
+    fig, ax = plt.subplots(2, 1, figsize = (10,4))
+
+    # Plot the histogram   
+    ax[0].hist(var_data)
+    ax[0].set_ylabel('Frequency')
+
+    # Add lines for the mean, median, and mode
+    ax[0].axvline(x=min_val, color = 'gray', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=mean_val, color = 'cyan', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=med_val, color = 'red', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=mod_val, color = 'yellow', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=max_val, color = 'gray', linestyle='dashed', linewidth = 2)
+
+    # Plot the boxplot   
+    ax[1].boxplot(var_data, vert=False)
+    ax[1].set_xlabel('Value')
+
+    # Add a title to the Figure
+    fig.suptitle('Data Distribution')
+
+    # Show the figure
+    plt.show()
+    fig.show()
+
+
+
+# show_distribution(df_students['Grade'])
+
+# show_distribution(df_students['StudyHours'])
+
+col = df_students[df_students['StudyHours'] > 1]['StudyHours']
+# show_distribution(col)
+q01 = df_students['StudyHours'].quantile(0.01)
+col = df_students[df_students['StudyHours'] > q01]['StudyHours']
+
+
+
+def show_density(var_data):
+    fig = plt.figure(figsize=(10,4))
+    # plot density
+    var_data.plot.density()
+
+
+    # add titles and labels
+    plt.title("Data Density")
+
+    # show the mean,median and mode 
+    plt.axvline(var_data.mean(), color = 'cyan', linestyle='dashed',linewidth=2)
+    plt.axvline(var_data.median(), color = 'red', linestyle='dashed',linewidth=2)
+    plt.axvline(var_data.mode()[0], color = 'orange', linestyle='dashed',linewidth=2)
+
+    plt.show()
+
+show_density(col)
+
+
 # visualize the data on the graph
 # we can add color, title, labels, grid and rotate the x markers inside the graph
 # plt.bar(x=df_students['Name'],height=df_students['Grade'], color="orange")
@@ -55,8 +121,8 @@ df_students = pd.concat([df_students,passes.rename('Pass')],axis=1)
 
 # Measures of central tendency
 
-# #save grades in a variable to calculate median , mode , mean
-var_grades = df_students['Grade']
+# # #save grades in a variable to calculate median , mode , mean
+# var_grades = df_students['Grade']
 
 # # get statitics
 # min_val = var_grades.min()
@@ -93,11 +159,11 @@ var_grades = df_students['Grade']
 # fig.show()
 
 
-fig = plt.figure(figsize=(10,4))
+# fig = plt.figure(figsize=(10,4))
 
-plt.boxplot(var_grades)
+# plt.boxplot(var_grades)
 
-plt.title('Data Distribution')
+# plt.title('Data Distribution')
 
-plt.show()
-fig.show()
+# plt.show()
+# fig.show()
