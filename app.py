91775b09401d5fa61ab7b40a1d9c372d4b40fa80
satinder <satindersingh472@gmail.com>
2023-07-05 11:19:43 -0600

    filtering_and_cleaning_data_mean_learned

--- app.py
+++ app.py
@@ -3,16 +3,14 @@ import pandas as pd
 
 # an array of grades 
 data = [50,50,47,97,49,3,53,42,26,74,82,62,37,15,70,27,36,35,48,52,63,64]
-# print(data)
+
+#modifiy an array into numpy array so that we can mathematical calculations easily
 grades = np.array(data)
-# print(grades)
 
-# print(type(data), 'x 2', data * 2)
-# print(type(grades), 'x 2', grades * 2)
-
+#with shape we can have an idea about the dimensions of an array
 grades.shape
 
-# print(grades.mean())
+# array.mean() can help find the mean of an array
 
 # an array of time devoted to study
 study_hours = [10.0,11.5,9.0,16.0,9.25,1.0,11.5,9.0,8.5,14.5,15.5,
@@ -21,6 +19,7 @@ study_hours = [10.0,11.5,9.0,16.0,9.25,1.0,11.5,9.0,8.5,14.5,15.5,
 # create 2d array 
 student_data = np.array([study_hours,grades])
 # print(student_data.shape)
+# array can be accessed with regular postion method 
 # print(student_data[0][0])
 
 avg_study = student_data[0].mean()
@@ -38,4 +37,29 @@ df_students = pd.DataFrame({'Name': ['Dan', 'Joann', 'Pedro', 'Rosie', 'Ethan', 
 # print(df_students.query('Name == "Rajab"'))
 
 df_students = pd.read_csv('grades.csv', delimiter=',',header='infer')
-print(df_students.head())
+# print(df_students.head())
+
+#handling missing values in data
+# print(df_students[df_students.isnull().any(axis =1)])
+#after getting empty values we can replace missing values with mean value of an array
+
+df_students['StudyHours'] = df_students['StudyHours'].fillna(df_students['StudyHours'].mean())
+
+# print(df_students)
+
+#drop the rows with null values
+df_students = df_students.dropna(axis=0,how='any')
+# print(df_students)
+
+#get the mean of study hours using the column name as an index
+mean_study = df_students['StudyHours'].mean()
+# print(mean_study)
+
+#get the mean of grades using the column name as a property
+mean_grade = df_students.Grade.mean()
+# print(mean_grade)
+
+
+# print the mean study and grades
+
+print('Average weekly study hours: {:.2f}\nAverage Grade: {:.2f}'.format(mean_study,mean_grade))
