0f86875907008ba1c6dbcbb563401e891462255c
satinder <satindersingh472@gmail.com>
2023-07-05 13:53:07 -0600

    learned_to_show_two_graphs

--- matplotlib_practice.py
+++ matplotlib_practice.py
@@ -17,10 +17,22 @@ plt.title("Student Grades")
 plt.xlabel('Student')
 plt.ylabel('Grade')
 plt.grid(color="#95a5a6",linestyle='--', linewidth=2,axis='y',alpha=0.7)
-plt.xticks(rotation=45)
-
+plt.show()
 
 fig,ax = plt.subplots(1,2,figsize=(10,4))
+# create a bar plot of name vs grade on the first axis
+ax[0].bar(x=df_students['Name'], height=df_students['Grade'], color="orange")
+ax[0].set_title('Grades')
+ax[0].set_xticklabels(df_students['Name'], rotation=90)
+
+# create a pie chart of pass counts
+pass_counts = df_students['Pass'].value_counts()
+ax[1].pie(pass_counts, labels=pass_counts)
+ax[1].set_title('Passing Grades')
+ax[1].legend(pass_counts.keys().tolist())
+
+fig.suptitle('Student Data')
+
 
 fig.show()
 
