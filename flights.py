73cb866683e9de46d1f9bae7753f2c9c7ab4336a
satinder <satindersingh472@gmail.com>
2023-07-06 11:20:56 -0600

    practiced_working_with_dataframe

--- flights.py
+++ flights.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
@@ -7,5 +8,25 @@ df_flights = pd.read_csv('data/flights.csv',delimiter=',',header='infer')
 df_flights['DepDel15'] = df_flights['DepDel15'].fillna(0)
 
 # print(df_flights.isnull().sum())
+groups = df_flights.groupby(df_flights['ArrDel15'] == 1)
 
-print(df_flights)
+flights_delayed = 0
+flight_ontime = 0
+
+for group in groups:
+    if group[0] == True:
+        flights_delayed = len(group[1])
+    else:
+        flight_ontime = len(group[1])
+
+
+
+print('Flights on time: {}\nFlights delayed: {}'.format(flight_ontime,flights_delayed))
+print(df_flights['ArrDel15'].mean())
+
+delay_counts = df_flights['ArrDel15'].value_counts()
+plt.pie(delay_counts,labels=delay_counts,autopct='%1.1f%%')
+plt.title('Flights get delayed by airports')
+plt.show()
+
+# plt.pie(x=df_flights['ArrDel15'],height=len(df_flights))
