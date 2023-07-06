c85a0973f1a8b8f1c133a11f505ecce0f25cbb61
satinder <satindersingh472@gmail.com>
2023-07-05 21:20:57 -0600

    working_scaler

--- matplotlib_practice.py
+++ matplotlib_practice.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+import scipy.stats as stats
+from sklearn.preprocessing import MinMaxScaler
+
 
 
 #load the data from csv file
@@ -83,6 +86,70 @@ for col_name in ['StudyHours','Grade']:
     standard_deviation = col.std()
 
     print('{}\n- Range: {:.2f}\n- Variance: {:.2f}\n- Standard Deviation: {:.2f}'.format(col_name,range,variance,standard_deviation))
+
+
+
+# Get the Grade column
+col = df_students['Grade']
+
+# get the density
+density = stats.gaussian_kde(col)
+
+# Plot the density
+col.plot.density()
+
+# Get the mean and standard deviation
+s = col.std()
+m = col.mean()
+
+# Annotate 1 stdev
+x1 = [m-s, m+s]
+y1 = density(x1)
+plt.plot(x1,y1, color='magenta')
+plt.annotate('1 std (68.26%)', (x1[1],y1[1]))
+
+# Annotate 2 stdevs
+x2 = [m-(s*2), m+(s*2)]
+y2 = density(x2)
+plt.plot(x2,y2, color='green')
+plt.annotate('2 std (95.45%)', (x2[1],y2[1]))
+
+# Annotate 3 stdevs
+x3 = [m-(s*3), m+(s*3)]
+y3 = density(x3)
+plt.plot(x3,y3, color='orange')
+plt.annotate('3 std (99.73%)', (x3[1],y3[1]))
+
+# Show the location of the mean
+plt.axvline(col.mean(), color='cyan', linestyle='dashed', linewidth=1)
+
+plt.axis('off')
+
+# plt.show()
+
+
+# common technique when dealing with numeric data is to normalize the data so that the
+# values retain their proportional distribution but are measured on the same scale
+# we will use minmax scaling technique that distributes the value proportionally on a scale of 0 to 1
+# we will use the scikit learn library's scalar method
+df_sample = df_students[df_students['StudyHours'] > 1]
+print(df_sample)
+df_sample.plot(x="Name",y=['Grade','StudyHours'], kind='bar',figsize=(8,5))
+plt.show()
+# get a scalar object
+scalar = MinMaxScaler()
+# create a new dataframe for the scaled values
+df_normalized = df_sample[['Name','Grade','StudyHours']].copy()
+
+# normalize the numeric columns
+df_normalized['Grade','StudyHours'] = scalar.fit_transform(df_normalized[['Grade','StudyHours']])
+
+# plot the normalized values
+df_normalized.plot(x='Name',y=['Grade','StudyHours'],kind='bar',figsize=(8,5))
+
+
+
+
 
 
 # visualize the data on the graph
