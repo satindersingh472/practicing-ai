0b71df29851fa1583782dc3270a9fbd974103dc2
satinder <satindersingh472@gmail.com>
2023-07-05 23:34:37 -0600

    finished_real_world_data_problem

--- matplotlib_practice.py
+++ matplotlib_practice.py
@@ -79,51 +79,51 @@ def show_density(var_data):
 # show_density(col)
 
 
-for col_name in ['StudyHours','Grade']:
-    col = df_students[col_name]
-    range = col.max() - col.min()
-    variance = col.var()
-    standard_deviation = col.std()
+# for col_name in ['StudyHours','Grade']:
+#     col = df_students[col_name]
+#     range = col.max() - col.min()
+#     variance = col.var()
+#     standard_deviation = col.std()
 
-    print('{}\n- Range: {:.2f}\n- Variance: {:.2f}\n- Standard Deviation: {:.2f}'.format(col_name,range,variance,standard_deviation))
+#     print('{}\n- Range: {:.2f}\n- Variance: {:.2f}\n- Standard Deviation: {:.2f}'.format(col_name,range,variance,standard_deviation))
 
 
 
 # Get the Grade column
-col = df_students['Grade']
+# col = df_students['Grade']
 
-# get the density
-density = stats.gaussian_kde(col)
+# # get the density
+# density = stats.gaussian_kde(col)
 
-# Plot the density
-col.plot.density()
+# # Plot the density
+# col.plot.density()
 
-# Get the mean and standard deviation
-s = col.std()
-m = col.mean()
+# # Get the mean and standard deviation
+# s = col.std()
+# m = col.mean()
 
-# Annotate 1 stdev
-x1 = [m-s, m+s]
-y1 = density(x1)
-plt.plot(x1,y1, color='magenta')
-plt.annotate('1 std (68.26%)', (x1[1],y1[1]))
+# # Annotate 1 stdev
+# x1 = [m-s, m+s]
+# y1 = density(x1)
+# plt.plot(x1,y1, color='magenta')
+# plt.annotate('1 std (68.26%)', (x1[1],y1[1]))
 
-# Annotate 2 stdevs
-x2 = [m-(s*2), m+(s*2)]
-y2 = density(x2)
-plt.plot(x2,y2, color='green')
-plt.annotate('2 std (95.45%)', (x2[1],y2[1]))
+# # Annotate 2 stdevs
+# x2 = [m-(s*2), m+(s*2)]
+# y2 = density(x2)
+# plt.plot(x2,y2, color='green')
+# plt.annotate('2 std (95.45%)', (x2[1],y2[1]))
 
-# Annotate 3 stdevs
-x3 = [m-(s*3), m+(s*3)]
-y3 = density(x3)
-plt.plot(x3,y3, color='orange')
-plt.annotate('3 std (99.73%)', (x3[1],y3[1]))
+# # Annotate 3 stdevs
+# x3 = [m-(s*3), m+(s*3)]
+# y3 = density(x3)
+# plt.plot(x3,y3, color='orange')
+# plt.annotate('3 std (99.73%)', (x3[1],y3[1]))
 
-# Show the location of the mean
-plt.axvline(col.mean(), color='cyan', linestyle='dashed', linewidth=1)
+# # Show the location of the mean
+# plt.axvline(col.mean(), color='cyan', linestyle='dashed', linewidth=1)
 
-plt.axis('off')
+# plt.axis('off')
 
 # plt.show()
 
@@ -133,22 +133,56 @@ plt.axis('off')
 # we will use minmax scaling technique that distributes the value proportionally on a scale of 0 to 1
 # we will use the scikit learn library's scalar method
 df_sample = df_students[df_students['StudyHours'] > 1]
-print(df_sample)
+# print(df_sample)
 df_sample.plot(x="Name",y=['Grade','StudyHours'], kind='bar',figsize=(8,5))
-plt.show()
+# plt.show()
 # get a scalar object
 scalar = MinMaxScaler()
 # create a new dataframe for the scaled values
 df_normalized = df_sample[['Name','Grade','StudyHours']].copy()
 
 # normalize the numeric columns
-df_normalized['Grade','StudyHours'] = scalar.fit_transform(df_normalized[['Grade','StudyHours']])
+df_normalized[['Grade','StudyHours']] = scalar.fit_transform(df_normalized[['Grade','StudyHours']])
 
 # plot the normalized values
 df_normalized.plot(x='Name',y=['Grade','StudyHours'],kind='bar',figsize=(8,5))
+# plt.show()
+
+print(df_normalized['Grade'].corr(df_normalized['StudyHours']))
+
+#create a scatter plot
+
+df_sample.plot.scatter(title='Study time vs grade', x='StudyHours',y='Grade')
+# plt.show()
 
 
+df_regression = df_sample[['Grade','StudyHours']].copy()
 
+
+#get the regression slope and intercept
+m,b,r,p,se = stats.linregress(df_regression['StudyHours'],df_regression['Grade']) 
+print('slope: {:.4f}\n-intercept: {:.4f}'.format(m,b))
+print('So...\nf(x) = {:.4f} + {:.4f}'.format(m,b))
+# calculate the f(x) for each x (study hours) value
+df_regression['fx'] = (m * df_regression['StudyHours']) + b
+# calculate the error between f(x) and y (grade) value
+df_regression['error'] = df_regression['fx'] - df_regression['Grade']
+
+#create a scatter plot of grade vs study hours
+df_regression.plot.scatter(x='StudyHours',y='Grade')
+plt.plot(df_regression['StudyHours'],df_regression['fx'],color='cyan')
+# plt.show()
+
+# define a function based on our regression coefficients
+def f(x):
+    m = 6.3134
+    b = -17.9164
+    return m*x + b
+
+study_time = 14
+
+prediction = f(study_time)
+print(prediction)
 
 
 
